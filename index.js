75345ade268e16f6ffeda9aa7162b113fa9c0f28
aksm-ms <58936966+aksm-ms@users.noreply.github.com>
2020-11-19 10:15:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ try {
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
-  console.log(`get github context: ${github.context}`);
+  console.log(`get github context: ${JSON.stringify(github.context)}`);
 //   console.log(`prev step output: webapp-url: ${github.steps.context.webapps-deploy.outputs.webapp-url}`);
 } catch (error) {
   core.setFailed(error.message);
